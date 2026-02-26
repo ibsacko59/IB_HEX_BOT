05be65260b989100be7084ef8f801c97a8db0dfd
ibsacko59 <ibsacko59@gmail.com>
2026-02-26 18:04:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
     devName: "Ibrahim Sory Sacko",
     version: "1.0",
     sessionFile: "./session.json",
-    menuImage: "https://i.ibb.co/KcM77nr2/1771804016858.png",
+    // Remplacez par une image valide
+    menuImage: "https://i.ibb.co/KcM77nr2/1771804016858.png", // Mettez un vrai lien d'image
     port: process.env.PORT || 3000
 };
