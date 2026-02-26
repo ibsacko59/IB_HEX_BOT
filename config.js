ba19bda63701468fd309d5645d964a5e1f46ddfa
ibsacko59 <ibsacko59@gmail.com>
2026-02-26 17:15:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+    ownerNumber: "224621963059",
+    prefix: "Ib",
+    botName: "IB-HEX-BOT",
+    ownerName: "Ibrahima Sory Sacko",
+    devName: "Ibrahim Sory Sacko",
+    version: "1.0",
+    sessionFile: "./session.json",
+    menuImage: "https://i.ibb.co/KcM77nr2/1771804016858.png",
+    port: process.env.PORT || 3000
+};
